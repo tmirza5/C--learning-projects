9f18f75d141a797c3d685a16190f2fee9b3e0830
Muhammad Taqi Mirza <tmirza5@outlook.com>
2024-09-25 15:15:52 +0100

    Made a Project called "Practice" and added "Multiples3or5.cs"

---
+++ Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice
+{
+    class Multiples
+    {
+        public static void Main()
+        {
+            int total = 0;
+            for (int i=0; i<1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    total = total + i;
+                }
+                else if (i % 5 == 0)
+                {
+                    total = total + i;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
